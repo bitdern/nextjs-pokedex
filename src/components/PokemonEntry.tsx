0e0e6535acfebdd54aa1282e5f29351c3a8fa8df
aidan.newell <acnewell09@gmail.com>
2023-06-17 10:09:09 -0500

    styling the pokemon card entry

--- src/components/PokemonEntry.tsx
+++ src/components/PokemonEntry.tsx
@@ -2,6 +2,7 @@ import usePokemon from "@/hooks/usePokemon";
 import Link from "next/link";
 import styles from "@/styles/PokemonEntry.module.css";
 import { Spinner } from "react-bootstrap";
+import Image from "next/image";
 
 export default function PokemonEntry({ name }: { name: string }) {
   const { pokemon, pokemonLoading } = usePokemon(name);
@@ -13,6 +14,12 @@ export default function PokemonEntry({ name }: { name: string }) {
         {pokemon && (
           <div className={styles.card}>
             <h1 className="text-center text-capitalize">{pokemon.name}</h1>
+            <Image
+              src={pokemon.sprites.other["official-artwork"].front_default}
+              alt={"Pokemon" + pokemon.name}
+              width={200}
+              height={200}
+            />
           </div>
         )}
       </div>
